52257d24bb261bcd62b4afa137bc511fd3c98dc6
DavideBernardini <davidebernardini09@gmail.com>
2021-09-20 17:21:36 +0200

    html, css + struttura vue

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        image: {
+            'url': 'img/logo.png',
+            'name': 'boolean-logo'
+        },
+        todos: [
+            'Fare i compiti',
+            'Fare la spesa',
+            'Fare il bucato'
+        ]
+    },
+    methods: {
+
+    }
+})
